50c8f279a377016b8945e8614ec8e643b10a9e67
novakm <novakm@wwt.com>
2022-04-07 13:24:19 -0600

    rename index to App.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import { App } from "./components/App";
+import { App } from "./components/App/App";
 
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
