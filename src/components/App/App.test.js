df98cbb364c402f823c614c59b72a39d50679b18
novakm <novakm@wwt.com>
2022-04-12 14:02:52 -0600

    add linting with standard

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { App } from './App';
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { App } from './App'
+import { render, screen } from '@testing-library/react'
 
 describe('App', () => {
   it('should render a header', () => {
-    render(<App />);
-    const header = screen.getByRole('heading');
-    expect(header).toHaveTextContent('Hello world!');
-  });
-});
+    render(<App />)
+    const header = screen.getByRole('heading')
+    expect(header).toHaveTextContent('Hello world!')
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
-import { App } from "./components/App/App";
+import ReactDOM from 'react-dom'
+import { App } from './components/App/App'
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+const app = document.getElementById('app')
+ReactDOM.render(<App />, app)
